5e777d44ce7197cae93a91f7a423aee24d0ddc31
Alex Kiura <kiuraalex@gmail.com>
2016-02-02 15:01:22 +0300

    (feature) Add login page & navigation from index
    The login is yet to be supported by a User model

--- server.py
+++ server.py
@@ -1,15 +1,36 @@
 from flask import Flask, render_template
 from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import Required, Length, Email
+from flask.ext.wtf import Form
+from flask.ext.moment import Moment
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+
+class LoginForm(Form):
+    email = StringField('Email', validators=[Required(), Length(1, 64),
+                                             Email()])
+    password = PasswordField('Password', validators=[Required()])
+    remember_me = BooleanField('Keep me logged in')
+    submit = SubmitField('Log In')
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+@app.route('/login')
+def auth():
+    form = LoginForm()
+    return render_template('login.html', form=form)
 
 if __name__ == '__main__':
     manager.run()
