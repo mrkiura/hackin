6e0165de2daf302610b797e4ad517232560ca1bd
Alex Kiura <kiuraalex@gmail.com>
2016-02-02 17:41:01 +0300

    (chore) Add user creation & athentication tests

---
+++ tests/test_user_auth.py
@@ -0,0 +1,27 @@
+import unittest
+
+from app.server import User
+
+
+class UserModelTestCase(unittest.TestCase):
+    '''class to test user creation and
+    passwor verification
+    '''
+
+    def test_create_user(self):
+        user = User()
+        self.assertTrue(user is not None)
+
+    def test_set_password(self):
+        user = User(password='123')
+        self.assertTrue(user.hashed_pass is not None)
+
+    def test_get_password(self):
+        user = User(password='123')
+        with self.assertRaises(AttributeError):
+            user.password
+
+    def test_verify_password(self):
+        user = User(password='123')
+        self.assertTrue(user.verify_password('123'))
+        self.assertFalse(user.verify_password('321'))
