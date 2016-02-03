8b328c93ca5db9d3cfd4156c75e248faa1c25c3f
Alex Kiura <alex.kiura@andela.com>
2016-02-03 14:08:03 +0300

    (bug) Fix ref param passed to FirePad constructor
    ref was being passed as a function instead of a value returned
    from a function

--- app/static/js/editor.js
+++ app/static/js/editor.js
@@ -1,5 +1,33 @@
 // initialize data store and the editors
+var startEditor = function() {
+    var editor = ace.edit('firepad');
+    editor.$blockScrolling = Infinity;
+    editor.setTheme("ace/theme/monokai");
+    editor.setFontSize(14)
+    var session = editor.getSession();
+    session.setUseWrapMode(true);
+    session.setUseWorker(false);
+    session.setMode("ace/mode/python");
 
-var firepadRef = new Firebase('https://hack-in.firebaseio.com');
-var editor = ace.edit('firepad');
-var firepad = Firepad.fromACE(firepadRef, editor);
+    var firepad = Firepad.fromACE(getUserRef(), editor, {
+        defaultText: '# Hack in python'
+    });
+}
+
+var getUserRef = function() {
+   var fbRef = new Firebase('https://hack-in.firebaseio.com');
+      var hash = window.location.hash.replace(/#/g, '');
+      console.log(hash)
+      console.log(fbRef)
+      if (hash) {
+        fbRef = fbRref.child(hash);
+      } else {
+        fbRef = fbRef.push(); // generate unique location.
+        window.location = window.location + '#' + fbRef.key(); // add it as a hash to the URL.
+      }
+      if (typeof console !== 'undefined')
+        console.log('Firebase data: ', fbRef.toString());
+      return fbRef;
+}
+
+window.onload = startEditor;
